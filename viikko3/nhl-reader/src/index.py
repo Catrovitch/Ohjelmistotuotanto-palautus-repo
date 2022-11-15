c9bf2cd3609a6b21ed044aa79d4e6ebacf21e0e0
Alexander <ale.engelhardt@outlook.com>
2022-11-15 10:22:54 +0200

    exercise 2

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -20,9 +20,25 @@ def main():
             players.append(player)
 
     print("Players from FIN 2021-01-04:")
+    print("")
 
-    for player in players:
-        print(player)
+    players_by_goals = order_players_by_points(players)
+
+    for player in players_by_goals:
+        print(player_point_print(player))
+
+def order_players_by_points(player_lst):
+
+    return sorted(player_lst, key=lambda player: player_points(player), reverse=True)
+
+def player_points(player):
+    player.points = player.goals + player.assists
+    return player.goals+player.assists
+
+def player_point_print(player):
+
+    return f"{player.name}{' '*(25-len(player.name))}{player.team}{' '*(4-len(str(player.goals)))}{player.goals} + {player.assists}{' '*(3-len(str(player.assists)))} = {player.points}"
+
 
 
 if __name__ == "__main__":
